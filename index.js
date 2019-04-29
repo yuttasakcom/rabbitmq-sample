ac453330ff92fdc1d9273899852deb413ea67345
Yuttasak Pannawat <yuttasak_pan@true-e-logistics.com>
2019-04-29 15:50:01 +0700

    updated config

--- index.js
+++ index.js
@@ -1,45 +1,55 @@
 import Koa from 'koa'
-import rascal, {Broker} from 'rascal'
+import rascal, { Broker } from 'rascal'
 
 const app = new Koa()
 
 const port = process.env.PORT || "4000"
 
 const config = {
-    "vhosts": {
-      "v1": {
-        "connection": {
-          "slashes": true,
-          "protocol": "amqp",
-          "hostname": "localhost",
-          "user": "user",
-          "password": "password",
-          "port": 5672,
-          "vhost": "v1",
-          "options": {
-              "heartbeat": 5
-          },
-          "socketOptions": {
-              "timeout": 10000
-          }
+    vhosts: {
+        "/": {
+            connection: {
+                heartbeat: 1,
+                socketOptions: {
+                    timeout: 1000
+                },
+                user: "user",
+                password: "password"
+            },
+            exchanges: ["demo_ex"],
+            queues: ["demo_q"],
+            bindings: [
+                "demo_ex[a.b.c] -> demo_q"
+            ],
+            publications: {
+                "demo_pub": {
+                    "exchange": "demo_ex",
+                    "routingKey": "a.b.c"
+                }
+            },
+            subscriptions: {
+                "demo_sub": {
+                    "queue": "demo_q"
+                }
+            }
         }
-      }
     }
-  }
+}
 
   Broker.create(rascal.withDefaultConfig(config), (err, broker) => {
       if(err) throw err
 
       broker.on('error', console.error)
 
-      broker.publish('demo_publication', 'Hello World!', (err, publication) => {
-          if(err) throw err
-          publication.on('error', console.error)
+      broker.publish("demo_pub", "Hello World!", (err, publication) => {
+          if (err) throw err
+          publication.on("error", console.error)
       })
 
-      broker.subscribe('demo_subscription', (err, subscription) => {
+      broker.subscribe('demo_sub', (err, subscription) => {
           if(err) throw err
           subscription.on('message', (message, content, ackOrNack) => {
+            //   console.log(message)
               console.log(content)
               ackOrNack()
           })
